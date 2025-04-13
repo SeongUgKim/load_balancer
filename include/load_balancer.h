cdb51f12c2049a3adeb6566fd4cc3ac50c6d91d9
SeongUgKim <sukim2017@gmail.com>
2025-04-13 02:20:57 -0500

    first commit

---
+++ include/load_balancer.h
@@ -0,0 +1,13 @@
+#ifndef LOAD_BALANCER_H
+#define LOAD_BALANCER_H
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+
+#include <server.h>
+
+#endif // LOAD_BALANCER_H
+
---
+++ include/server.h
@@ -0,0 +1,7 @@
+#ifndef SERVER_H
+#define SERVER_H
+class Server 
+{
+
+};
+#endif // SERVER_H
